79f6ac5e20b3019b0a0396822102d21f5e67520a
Garrett Rieger <garrett.rieger@fcc-fac.ca>
2023-01-26 11:45:39 -0600

    task 4 commits

--- src/main/java/ca/garrett/githubactions/Main.java
+++ src/main/java/ca/garrett/githubactions/Main.java
@@ -2,7 +2,7 @@ package ca.garrett.githubactions;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Task 3 PR Trigger Test");
+        // load yaml file
+
     }
 }
