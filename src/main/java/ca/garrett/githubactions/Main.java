7f5b8ff6fcb3d0a3c4a0447ee5b4fab3e569902c
Garrett Rieger <garrett.rieger@fcc-fac.ca>
2023-01-26 09:27:07 -0600

    task 2 commits

--- src/main/java/ca/garrett/githubactions/Main.java
+++ src/main/java/ca/garrett/githubactions/Main.java
@@ -3,5 +3,6 @@ package ca.garrett.githubactions;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Java file change!");
     }
 }
