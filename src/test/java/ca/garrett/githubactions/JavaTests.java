125a3731eaea4518b4e9aa9f109c330f02db0dcc
Garrett Rieger <garrett.rieger@fcc-fac.ca>
2023-01-26 10:35:04 -0600

    task 3 commits

---
+++ src/test/java/ca/garrett/githubactions/JavaTests.java
@@ -0,0 +1,12 @@
+package ca.garrett.githubactions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JavaTests {
+
+    @Test
+    public void SampleTest() {
+        Assertions.assertTrue(true);
+    }
+}
